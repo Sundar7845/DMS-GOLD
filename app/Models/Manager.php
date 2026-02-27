dcfea29353db08082e6138d7817ec1f3ee6ea498
sundar7854 <prathip221@gmail.com>
2026-02-27 12:03:44 +0530

    Refactor code structure for improved readability and maintainability

--- app/Models/Manager.php
+++ app/Models/Manager.php
@@ -4,11 +4,10 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-use Laravel\Passport\HasApiTokens;
 
 class Manager extends Model
 {
-    use HasFactory, HasApiTokens;
+    use HasFactory;
 
     public $table = 'tbl_manager';
 
